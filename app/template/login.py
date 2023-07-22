f4f8ef1374f9cd147312413735c8e2ba844f97a7
Dat <dat.lemindast@gmail.com>
2023-07-22 16:25:54 +0700

    change

--- app/template/login.py
+++ app/template/login.py
@@ -9,11 +9,22 @@ class LoginUI(Frame):
     
     def initUI(self):
         self.parent.title("NOHCEL")
-        self.pack(fill = "X", side = "BOTH")
+        self.pack(fill = BOTH, side = TOP)
         
         self.label_privacy = Label(self.parent, text = "lcmd privacy 2023")
-        self.label_privacy.pack()
+        self.label_privacy.pack(fill = BOTH, side = BOTTOM)
     
-        self.notebook = Notebook(self)
-        self.notebook.pack()
-    
+        self.notebook = Notebook(self, width= 100, height= 120)
+        self.notebook.pack(fill = X, side = TOP, padx = 50, pady =50, ipadx= 200, ipady = 200)
+        
+        self.frame_login = Frame(self.notebook)
+        self.frame_login.pack(fill = BOTH, side = TOP)
+        
+        frame_ = [None for _ in range(4)]
+        for index in range(4):
+            frame_[index] = Frame(self.frame_login)
+            frame_[index].pack(side = TOP, fill = X)
+
+        Label_login = Label(frame_[0], text = "LOGIN")
+        Label_login.pack(fill = X, side = TOP)
+        
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import tkinter
 from tkinter import Tk
+from app.template.login import LoginUI
 
 if __name__ == "__main__":
-    pass
+    root = Tk()
+    root.geometry("1000x1000+100+100")
+    app = LoginUI(root)
+    root.mainloop()
+
