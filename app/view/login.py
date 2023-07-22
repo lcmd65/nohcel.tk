0893ce7b467bc9185c263610a33ece14b307b532
Dat <dat.lemindast@gmail.com>
2023-07-22 14:25:11 +0700

    change

---
+++ app/view/login.py
@@ -0,0 +1,13 @@
+import tkinter
+from tkinter import ttk, messagebox
+
+class LoginUI(tkinter.Frame):
+    def __init__(self, parent):
+        tkinter.Frame.__init__(self, parent)
+        self.parent = parent
+        self.initUI()
+    
+    def initUI(self):
+        
+        
+    
---
+++ main.py
@@ -0,0 +1,5 @@
+import tkinter
+from tkinter import Tk
+
+if __name__ == "__main__":
+    pass
