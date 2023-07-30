b2d6d0df699411fa7ed1288377f4402ed6b63723
Dat <dat.lemindast@gmail.com>
2023-07-30 15:54:16 +0700

    change

--- app/model/conversation.py
+++ app/model/conversation.py
@@ -9,9 +9,6 @@ class Statement():
         self._token = None
         self._annotations = []
         self._roll = None
-        
-    def tranferNumber(self, prefix, suffix):
-        return prefix * suffix
     
     def processingAnnotation(self):
         """ annotations processing step 1: embedded database"""
@@ -27,7 +24,8 @@ class Statement():
                         self._annotations.append(item)
                         
     def processingText(self):
-        with open("app/embedded_label.json", "r+") as file:
+        """ processing Acronyms  word"""
+        with open("app/embedded_word.json", "r+") as file:
             data = json.loads(file)
         self._token = nltk.tokenize(self.text)
         for index in range(len(self._token)):
@@ -37,6 +35,42 @@ class Statement():
                 elif index < len(self._token) - 1 and "".join([self._token[index], self._token[index + 1]]) == item:
                     self._token[index] = item.get()
                     self._token.pop(index+1)
+        file.close()
+
+    def processingNumberSuffix(self):
+        """ processing and normalize number """
+        with open("app/embedded_prefix.json", "r+") as prefix:
+            data_prefix = json.loads(prefix)
+        with open("app/embedded_suffix.json", "r+") as suffix:
+            data_suffix = json.loads(suffix)
+        for index in range(len(self._token) - 1):
+            if self._token[index] in data_prefix.key() and self._token[index + 1] in data_suffix.key():
+                self._token[index] = data_prefix[self._token[index]].get() * data_suffix[self._token[index + 1]].get()
+                self._token.pop(index + 1) 
+        prefix.close()
+        suffix.close()
+            
+    
+    def processingNumberContinue(self):
+        with open("app/embedded_prefix.json", "r+") as prefix:
+            data_prefix = json.loads(prefix)
+        for first_index in range(len(self._token)-1):
+            if self._token[first_index] in data_prefix.key():
+                back = data_prefix[self._token[first_index]].get()
+                temp_index = first_index
+                for second_index in range(first_index + 1, len(self._token)):
+                    if self._token[second_index] in data_prefix.key():
+                        back = back*10 + data_prefix[self._token[second_index]].get()
+                        temp_index = second_index
+                    else:
+                        break
+                if temp_index > first_index :
+                    self._token[first_index] = "".join([self._token[first_index]])
+                    self.pop(first_index+ 1, temp_index)
+            else:
+                pass
+    
+            
         
 
 class Audio(Statement):
