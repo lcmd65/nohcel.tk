851e97400149f951a58b94a38225a9cd1be42bd5
Dat <dat.lemindast@gmail.com>
2023-08-04 20:55:09 +0700

    change

---
+++ app/template/qt/home.py
@@ -0,0 +1,24 @@
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class Home(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.schoice(self.hello))
+
+    
--- app/view/view.py
+++ app/view/view.py
@@ -5,9 +5,12 @@ from tkinter import messagebox
 from PIL import ImageTk, Image
 import app.environment as env
 
+"""
+tkinter view
+"""
 def loginView():
     try:
-        from app.template.login import LoginUI
+        from app.template.tkinter.login import LoginUI
         env.root_main = Tk()
         env.root_main.geometry("1000x1000+100+100")
         app.view.var.background_view = ImageTk.PhotoImage(Image.open('app/images/background_login.png').resize((1092, 1080))) ##4213 × 4167
@@ -19,7 +22,7 @@ def loginView():
         
 
 def homeView():
-        from app.template.home import Home
+        from app.template.tkinter.home import Home
         env.root_main = Tk()
         env.root_main.geometry("1000x1000+100+100")
         app.view.var.background_view = ImageTk.PhotoImage(Image.open('app/images/background_login.png').resize((1092, 1080))) ##4213 × 4167
@@ -29,7 +32,7 @@ def homeView():
     
 def helpView():
     try:
-        from app.template.help import Help
+        from app.template.tkinter.help import Help
         env.root_temp = Toplevel()
         env.root_temp.geometry("400x600+100+100")
         app.vew.var.background_view_toplevel = ImageTk.PhotoImage(Image.open('app/images/background_login.png').resize((1092, 1080)))
@@ -40,7 +43,7 @@ def helpView():
 
 def loginEditView():
     try:
-        from app.template.login_edit import LoginEdit
+        from app.template.tkinter.login_edit import LoginEdit
         env.root_temp = Toplevel()
         env.root_temp.geometry("1000x1000+100+100")
         application_edit = LoginEdit(env.root_temp)
@@ -50,10 +53,21 @@ def loginEditView():
 
 def editView():
     try:
-        from app.template.edit import EditEnv
+        from app.template.tkinter.edit import EditEnv
         env.root_temp = Tk()
         env.root_temp.geometry("1000x1000+100+100")
         application_edit = EditEnv(env.root_temp)
         env.root_temp.mainloop()
+    except Exception as e:
+        messagebox.showerror(title = "Error", message = e)
+
+"""
+qt view
+"""
+
+def homeViewQT():
+    try:
+        from app.template.qt.home import Home
+        
     except Exception as e:
         messagebox.showerror(title = "Error", message = e)
